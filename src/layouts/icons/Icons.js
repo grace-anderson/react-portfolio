281a758a7e0c247b04b9d465f01cea7ddc25e85f
Grace <grace.anderson.au@gmail.com>
2022-07-09 16:46:29 +1000

    added links to footer icons

--- src/layouts/icons/Icons.js
+++ src/layouts/icons/Icons.js
@@ -5,9 +5,27 @@ class Icons extends Component {
   render() {
     return (
       <div className="icon-style">
+        <a
+          href="https://github.com/grace-anderson"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaGithubSquare className="icon-padding" />
-          <FaLinkedin className="icon-padding"/>
-          <FaStackOverflow className="icon-padding"/>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/helenganderson/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin className="icon-padding" />
+        </a>
+        <a
+          href="https://stackexchange.com/users/20426110/hga"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaStackOverflow className="icon-padding" />
+        </a>
       </div>
     );
   }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div><h1>About</h1>
-    <p>
-
-    WHEN I load the portfolio the first time
-THEN the About Me title and section are selected by default
-</p><p>
-WHEN I am presented with the About Me section
-THEN I see a recent photo or avatar of the developer and a short bio about them
-
-    </p>
-    
+    <div>
+      <h1>About</h1>
+      <p>
+        WHEN I load the portfolio the first time THEN the About Me title and
+        section are selected by default
+      </p>
+      <p>
+        WHEN I am presented with the About Me section THEN I see a recent photo
+        or avatar of the developer and a short bio about them
+      </p>
     </div>
-  )
+  );
 }
