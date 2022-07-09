d72a8f2c9ea266bcba128721aaa83baefdc3f102
Grace <grace.anderson.au@gmail.com>
2022-07-09 17:50:10 +1000

    Completed image and text on About page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,26 @@
 import React from "react";
+import helen from "../components/images/helen.jpg";
 
 export default function Home() {
   return (
     <div>
       <h1>About Me</h1>
+      <div className="flex items-center justify-center">
+        <img className="image-circle" src={helen} alt="Helen"></img>
+      </div>
       <p>
-        WHEN I load the portfolio the first time THEN the About Me title and
-        section are selected by default
+        I'm a functional analyst with experience in a wide range of domains
+        (airline, university, federal government). I've  recently added
+        full-stack development to my skill set. I'm open to opportunities to use
+        my growing development expertise and learn new technologies.
       </p>
       <p>
-        WHEN I am presented with the About Me section THEN I see a recent photo
-        or avatar of the developer and a short bio about them
+        With over 15 years experience, I bring a comprehensive perspective on
+        software projects and application development as an analytical problem
+        solver enthusiastic about collaboratively meeting business needs. My
+        strengths in teamwork, strategic and design thinking, business process
+        analysis and improvement, requirements elicitation and system delivery
+        (waterfall and agile delivery contexts).
       </p>
     </div>
   );
