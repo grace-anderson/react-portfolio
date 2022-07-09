2bf2ba53e2b42ae293637bee5d468b01cef4b162
Grace <grace.anderson.au@gmail.com>
2022-07-10 06:07:09 +1000

    completed main components of portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { FaGithubSquare } from "react-icons/fa";
-import bookNest from "../components/images/bookNest.jpg";
+import bookNest from "../components/images/bookNest.png";
 import jate from "../components/images/jate.png";
+import social from "../components/images/social.png";
+import techBlog from "../components/images/techBlog.png";
+import ecommerce from "../components/images/ecommerce.png";
+import employeeTracker from "../components/images/employeeTracker.png";
 
 function Portfolio() {
   const portfolioItems = [
@@ -17,7 +21,7 @@ function Portfolio() {
       ),
       title: "Book Nest",
       description:
-        "A free book sharing app. Full-stack Javascript, Node, mySQL, sequelize, express, handlebars",
+        "A free book sharing app. Full-stack javascript, node, mySQL, sequelize, express, handlebars, express session, bcrypt",
       deployed: (
         <a
           href="https://the-book-nest.herokuapp.com/"
@@ -39,10 +43,22 @@ function Portfolio() {
       ),
     },
     {
-      image: <img className="card-image" src={jate} alt="JATE-Just Another Text Editor"></img>,
+      image: (
+        <a
+          href="https://mighty-inlet-78059.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="card-image"
+            src={jate}
+            alt="JATE-Just Another Text Editor"
+          ></img>
+        </a>
+      ),
       title: "JATE - Just Another Text Editor",
       description:
-        "A text editor that runs in the browser that will also function offline",
+        "A text editor that runs in the browser that also functions offline. Code-mirror, express, idb, if-env, node.",
       deployed: (
         <a
           href="https://mighty-inlet-78059.herokuapp.com/"
@@ -63,6 +79,150 @@ function Portfolio() {
         </a>
       ),
     },
+    {
+      image: (
+        <a
+          href="https://github.com/grace-anderson/social-network-api"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="card-image"
+            src={social}
+            alt="Social Network API"
+          ></img>
+        </a>
+      ),
+      title: "Social Network API",
+      description:
+        "Users share thoughts, react to friends’ thoughts, and create friend lists. MongoDB, Mongoose ODM, Express ",
+      deployed: (
+        <a
+          href="https://drive.google.com/file/d/1nO1uHivz6A1PZkcwcAJ_Y70_6Gppukte/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          alt="Demo of Social Network API"
+        >
+          Demonstration of Social Network API
+        </a>
+      ),
+      github: (
+        <a
+          href="https://github.com/grace-anderson/social-network-api"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithubSquare />
+        </a>
+      ),
+    },
+    {
+      image: (
+        <a
+          href="https://enigmatic-bayou-29924.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="card-image" src={techBlog} alt="Tech Blog app"></img>
+        </a>
+      ),
+      title: "Tech Blog",
+      description:
+        "A CMS-style blog where developers publish blog posts and comment on others' posts. Express, MySQL, sequelize",
+      deployed: (
+        <a
+          href="https://enigmatic-bayou-29924.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+          alt="Deployed Tech Blog app"
+        >
+          Deployed Tech Blog app
+        </a>
+      ),
+      github: (
+        <a
+          href="https://github.com/grace-anderson/tech-blog"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithubSquare />
+        </a>
+      ),
+    },
+    {
+      image: (
+        <a
+          href="https://github.com/grace-anderson/e-commerce-back-end"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="card-image"
+            src={ecommerce}
+            alt="E-commerce Back End"
+          ></img>
+        </a>
+      ),
+      title: "E-commerce Back End",
+      description:
+        "A back end for an e-commerce site configured with working Express.js API using Sequelize to interact with a MySQL database",
+      deployed: (
+        <a
+          href="https://drive.google.com/file/d/1uElJcKe_BD67qOa6dpZX5QPM1EucG4sa/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          alt="Demo of E-commerce Back End"
+        >
+          Demonstration of E-Commerce Back End
+        </a>
+      ),
+      github: (
+        <a
+          href="https://github.com/grace-anderson/e-commerce-back-end"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithubSquare />
+        </a>
+      ),
+    },
+    {
+      image: (
+        <a
+          href="https://github.com/grace-anderson/employee-tracker"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="card-image"
+            src={employeeTracker}
+            alt="Employee Tracker"
+          ></img>
+        </a>
+      ),
+      title: "Employee Tracker",
+      description:
+        "A command-line content management system (CMS) used to manage a company's employee database. MySQL, npm inquirer.",
+      deployed: (
+        <a
+          href="https://drive.google.com/file/d/1bkk5zaaNlpFWNYM8t2c8oHwZZ_kqIJN3/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          alt="Demo of Employee Tracker"
+        >
+          Demonstration of Employee Tracker
+        </a>
+      ),
+      github: (
+        <a
+          href="https://github.com/grace-anderson/employee-tracker"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithubSquare />
+        </a>
+      ),
+    },
   ];
 
   return (
@@ -73,9 +233,7 @@ function Portfolio() {
           <div key={item.projectTitle} className="card">
             <div class="container">
               <div>{item.image}</div>
-              <h4>
-                <b>{item.title}</b>
-              </h4>
+              <h3>{item.title}</h3>
               <p>{item.description}</p>
               <div>{item.deployed}</div>
               <div className="card-icon">{item.github}</div>
